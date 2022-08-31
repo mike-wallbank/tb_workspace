c4468d8caf1fe68219d28f0d244d22d7b690b9b9
Mike Wallbank <wallbank@fnal.gov>
2022-08-31 11:49:29 -0500

    updates to GAN: batch normalization, kernel initialization, save images of models

--- gan/gan-tb.py
+++ gan/gan-tb.py
@@ -9,6 +9,7 @@ from numpy import vstack
 from numpy import array
 from numpy.random import randn
 from numpy.random import randint
+from tensorflow.keras.initializers import RandomNormal
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -18,18 +19,23 @@ from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Conv2DTranspose
 from tensorflow.keras.layers import LeakyReLU
 from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.utils.vis_utils import plot_model
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import h5py
 
 # define the standalone discriminator model
-def define_discriminator(in_shape=(100,80,1)):
+def define_discriminator(in_shape=(32,32,1)):
+    init = RandomNormal(stddev=0.02)
     model = Sequential()
-    model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same', input_shape=in_shape))
+    model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same', kernel_initializer=init, input_shape=in_shape))
+    model.add(BatchNormalization(momentum=0.8))
     model.add(LeakyReLU(alpha=0.2))
     model.add(Dropout(0.4))
-    model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same'))
+    model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same', kernel_initializer=init))
+    model.add(BatchNormalization(momentum=0.8))
     model.add(LeakyReLU(alpha=0.2))
     model.add(Dropout(0.4))
     model.add(Flatten())
@@ -38,26 +44,38 @@ def define_discriminator(in_shape=(100,80,1)):
     #opt = SGD(lr=0.0002, momentum=0.5)
     opt = Adam(lr=0.0002, beta_1=0.5)
     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+    print("Discriminator model:")
+    d_model.summary()
+    plot_model(d_model, to_file='logs/discriminator_plot.png', show_shapes=True, show_layer_names=True)
     return model
 
 # define the standalone generator model
 def define_generator(latent_dim):
+    init = RandomNormal(stddev=0.02)
     model = Sequential()
     # foundation for 25x20 image
     # 128 is the number of images which can be generated, in the convolutional layer
-    n_nodes = 128 * 25 * 20
-    model.add(Dense(n_nodes, input_dim=latent_dim))
+    n_nodes = 128 * 8 * 8
+    model.add(Dense(n_nodes, kernel_initializer=init, input_dim=latent_dim))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Reshape((25, 20, 128)))
+    model.add(Reshape((8, 8, 128)))
     # upsample to 50x40
-    model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
+    model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same', kernel_initializer=init))
+    model.add(BatchNormalization(momentum=0.8))
     model.add(LeakyReLU(alpha=0.2))
     # upsample to 100x80
-    model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
+    model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same', kernel_initializer=init))
+    model.add(BatchNormalization(momentum=0.8))
     model.add(LeakyReLU(alpha=0.2))
     # create single output feature map (the actual generated image)
     # preserve dimensions so use a kernel which is a multiple
-    model.add(Conv2D(1, (25,20), activation='tanh', padding='same'))
+    model.add(Conv2D(1, (8,8), activation='tanh', padding='same', kernel_initializer=init))
+
+    print("Generator model:")
+    g_model.summary()
+    plot_model(g_model, to_file='logs/generator_plot.png', show_shapes=True, show_layer_names=True)
+
     return model
 
 # define the combined generator and discriminator model, for updating the generator
@@ -74,6 +92,11 @@ def define_gan(g_model, d_model):
     #opt = SGD(lr=0.0002, momentum=0.5)
     opt = Adam(lr=0.0002, beta_1=0.5)
     model.compile(loss='binary_crossentropy', optimizer=opt)
+    #model.compile(loss='mean_squared_error', optimizer=opt)
+
+    print("GAN model:")
+    gan_model.summary()
+
     return model
 
 # load cvn maps
@@ -81,6 +104,8 @@ def load_real_samples():
     inFile = h5py.File('h5_train/gan_reco_20000_TrainData.h5caf.h5', 'r')
     pm = array(inFile['cvnmap'])
     pm_shape = pm.reshape(pm.shape[0], 2, 100, 80)
+    # try reducing to 32x32
+    pm_shape = pm_shape[:,:,0:32,24:56]
     pm_xz, pm_yz = (expand_dims(squeeze(split(pm_shape, 2, axis=1)), axis=-1).astype('float32') - 127.5) / 127.5
     return pm_xz
 
@@ -123,7 +148,7 @@ def save_plot(examples_fake, examples_real, epoch, n=10):
         # plot raw pixel data
         plt.imshow(examples_fake[i, :, :, 0], cmap='gray_r')
     # save plot to file
-    filename = 'generated_plot_e%03d.png' % (epoch+1)
+    filename = 'logs/generated_plot_e%04d.png' % (epoch+1)
     plt.savefig(filename)
     plt.clf()
     for i in range(n * n):
@@ -134,8 +159,9 @@ def save_plot(examples_fake, examples_real, epoch, n=10):
         # plot raw pixel data
         plt.imshow(examples_real[i, :, :, 0], cmap='gray_r')
     # save plot to file
-    filename = 'trainexamples_plot_e%03d.png' % (epoch+1)
+    filename = 'logs/trainexamples_plot_e%04d.png' % (epoch+1)
     plt.savefig(filename)
+    plt.clf()
 
 # evaluate the discriminator, plot generated images, save generator model
 def summarize_performance(epoch, g_model, d_model, dataset, latent_dim, n_samples=100):
@@ -152,7 +178,7 @@ def summarize_performance(epoch, g_model, d_model, dataset, latent_dim, n_sample
     # save plot
     save_plot(x_fake, X_real, epoch)
     # save the generator model tile file
-    filename = 'generator_model_%03d.h5' % (epoch + 1)
+    filename = 'logs/generator_model_%04d.h5' % (epoch + 1)
     g_model.save(filename)
 
 # create a line plot of loss for the gan and save to file
@@ -171,7 +197,7 @@ def plot_history(d_loss_real_history, d_acc_real_history,
     plt.plot(d_acc_fake_history, label='d-acc (fake)')
     plt.legend()
     # save plot to file
-    plt.savefig('loss_acc.png')
+    plt.savefig('logs/loss_acc.png')
     plt.close()
 
 # train the generator and discriminator
@@ -207,25 +233,19 @@ def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=100, n_batc
             d_acc_fake_history.append(d_acc_fake)
             g_loss_history.append(g_loss)
         # evaluate the model performance, sometimes
-        if (i+1) % 1 == 0:
+        if (i+1) % 100 == 0:
             summarize_performance(i, g_model, d_model, dataset, latent_dim)
-        plot_history(d_loss_real_history, d_acc_real_history, d_loss_fake_history, d_acc_fake_history, g_loss_history)
+    plot_history(d_loss_real_history, d_acc_real_history, d_loss_fake_history, d_acc_fake_history, g_loss_history)
 
 # size of the latent space
 latent_dim = 100
 # create the discriminator
 d_model = define_discriminator()
-print("Discriminator model:")
-d_model.summary()
 # create the generator
 g_model = define_generator(latent_dim)
-print("Generator model:")
-g_model.summary()
 # create the gan
 gan_model = define_gan(g_model, d_model)
-print("GAN model:")
-gan_model.summary()
 # load image data
 dataset = load_real_samples()
 # train model
-train(g_model, d_model, gan_model, dataset, latent_dim, 10)
+train(g_model, d_model, gan_model, dataset, latent_dim, 1000)
